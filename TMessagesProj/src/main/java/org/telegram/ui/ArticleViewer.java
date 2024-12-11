2f573b9a5464f5c4e98dd156ddb1a353fbad2dbe
dic1911 <d69873@gmail.com>
2024-12-11 16:44:36 +0800

    fix: hide broken scroll progress

--- TMessagesProj/src/main/java/org/telegram/ui/ArticleViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ArticleViewer.java
@@ -13429,7 +13429,8 @@ public class ArticleViewer implements NotificationCenter.NotificationCenterDeleg
 
         public float overrideProgress = -1f;
         public float getProgress() {
-            if (isArticle()) {
+            // 030: top progress is broken for bottom action bar
+            if (isArticle() && !BOTTOM_ACTION_BAR) {
                 if (overrideProgress >= 0) return overrideProgress;
                 int first = layoutManager.findFirstVisibleItemPosition();
                 View view = layoutManager.findViewByPosition(first);
