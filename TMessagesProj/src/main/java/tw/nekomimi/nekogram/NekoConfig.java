639357be4a4d1bfef5666f9ee5d27b9592f6eb44
dic1911 <d69873@gmail.com>
2025-03-06 13:02:24 +0800

    misc: remove problematic config to prevent crash on launch

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/NekoConfig.java
@@ -375,7 +375,6 @@ public class NekoConfig {
         configs.add(a);
         return a;
     }
-
     public static void loadConfig(boolean force) {
         synchronized (sync) {
             if (configLoaded && !force) {
@@ -384,53 +383,58 @@ public class NekoConfig {
             for (int i = 0; i < configs.size(); i++) {
                 ConfigItem o = configs.get(i);
 
-                if (o.type == configTypeBool) {
-                    o.value = preferences.getBoolean(o.key, (boolean) o.defaultValue);
-                }
-                if (o.type == configTypeInt) {
-                    try {
-                        o.value = preferences.getInt(o.key, (int) o.defaultValue);
-                    } catch (Exception e) {
-                        Log.e("030-ex", String.format("error while loading %s : %s", String.valueOf(o.key), String.valueOf(o.defaultValue)), e);
-                        o.value = 0;
+                try {
+                    if (o.type == configTypeBool) {
+                        o.value = preferences.getBoolean(o.key, (boolean) o.defaultValue);
                     }
-                }
-                if (o.type == configTypeLong) {
-                    o.value = preferences.getLong(o.key, (Long) o.defaultValue);
-                }
-                if (o.type == configTypeFloat) {
-                    o.value = preferences.getFloat(o.key, (Float) o.defaultValue);
-                }
-                if (o.type == configTypeString) {
-                    o.value = preferences.getString(o.key, (String) o.defaultValue);
-                }
-                if (o.type == configTypeSetInt) {
-                    Set<String> ss = preferences.getStringSet(o.key, new HashSet<>());
-                    HashSet<Integer> si = new HashSet<>();
-                    for (String s : ss) {
-                        si.add(Integer.parseInt(s));
-                    }
-                    o.value = si;
-                }
-                if (o.type == configTypeMapIntInt) {
-                    String cv = preferences.getString(o.key, "");
-                    // Log.e("NC", String.format("Getting pref %s val %s", o.key, cv));
-                    if (cv.length() == 0) {
-                        o.value = new HashMap<Integer, Integer>();
-                    } else {
+                    if (o.type == configTypeInt) {
                         try {
-                            byte[] data = Base64.decode(cv, Base64.DEFAULT);
-                            ObjectInputStream ois = new ObjectInputStream(
-                                    new ByteArrayInputStream(data));
-                            o.value = (HashMap<Integer, Integer>) ois.readObject();
-                            if (o.value == null) {
-                                o.value = new HashMap<Integer, Integer>();
-                            }
-                            ois.close();
+                            o.value = preferences.getInt(o.key, (int) o.defaultValue);
                         } catch (Exception e) {
-                            o.value = new HashMap<Integer, Integer>();
+                            Log.e("030-ex", String.format("error while loading %s : %s", String.valueOf(o.key), String.valueOf(o.defaultValue)), e);
+                            o.value = 0;
                         }
                     }
+                    if (o.type == configTypeLong) {
+                        o.value = preferences.getLong(o.key, (Long) o.defaultValue);
+                    }
+                    if (o.type == configTypeFloat) {
+                        o.value = preferences.getFloat(o.key, (Float) o.defaultValue);
+                    }
+                    if (o.type == configTypeString) {
+                        o.value = preferences.getString(o.key, (String) o.defaultValue);
+                    }
+                    if (o.type == configTypeSetInt) {
+                        Set<String> ss = preferences.getStringSet(o.key, new HashSet<>());
+                        HashSet<Integer> si = new HashSet<>();
+                        for (String s : ss) {
+                            si.add(Integer.parseInt(s));
+                        }
+                        o.value = si;
+                    }
+                    if (o.type == configTypeMapIntInt) {
+                        String cv = preferences.getString(o.key, "");
+                        // Log.e("NC", String.format("Getting pref %s val %s", o.key, cv));
+                        if (cv.length() == 0) {
+                            o.value = new HashMap<Integer, Integer>();
+                        } else {
+                            try {
+                                byte[] data = Base64.decode(cv, Base64.DEFAULT);
+                                ObjectInputStream ois = new ObjectInputStream(
+                                        new ByteArrayInputStream(data));
+                                o.value = (HashMap<Integer, Integer>) ois.readObject();
+                                if (o.value == null) {
+                                    o.value = new HashMap<Integer, Integer>();
+                                }
+                                ois.close();
+                            } catch (Exception e) {
+                                o.value = new HashMap<Integer, Integer>();
+                            }
+                        }
+                    }
+                } catch (Exception ex) {
+                    preferences.edit().remove(o.key).commit();
+                    Log.e("030-cfg", String.format("removed %s from config due to error", o.key), ex);
                 }
             }
             configLoaded = true;
