9e3cc002ca5e66d33f7b2cff837338755b2da9b5
dic1911 <d69873@gmail.com>
2025-05-28 17:31:37 +0800

    fix ffmpeg build err

--- TMessagesProj/jni/build_ffmpeg_clang.sh
+++ TMessagesProj/jni/build_ffmpeg_clang.sh
@@ -86,7 +86,7 @@ function build_one {
 	--enable-protocol=file \
 	--enable-decoder=opus \
 	--enable-decoder=h264 \
-	--enable-decoder=h265 \
+	--enable-decoder=hevc \
 	--enable-decoder=mpeg4 \
 	--enable-decoder=mjpeg \
 	--enable-decoder=gif \
