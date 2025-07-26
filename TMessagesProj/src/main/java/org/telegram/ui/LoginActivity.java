94aee2f74e5436675f6567bb6973412124305388
dic1911 <d69873@gmail.com>
2025-07-26 20:32:22 +0800

    Revert "misc: hide custom backend for users"
    This reverts commit 68b747704104e2eb8771bcb13b898a356254ad3e.

--- TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LoginActivity.java
@@ -814,7 +814,7 @@ public class LoginActivity extends BaseFragment implements NotificationCenter.No
                     else
                         phoneView.testBackendCheckBox.setVisibility(View.GONE);
                 }
-                if (phoneView.customBackendCheckBox != null && NekoXConfig.isDeveloper()) {
+                if (phoneView.customBackendCheckBox != null) {
                     phoneView.customBackendCheckBox.setVisibility(
                             phoneView.customBackendCheckBox.getVisibility() == View.GONE ?
                                     View.VISIBLE : View.GONE);
