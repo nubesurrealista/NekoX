9c62c09ac2b4e1df22d29c9b609948433bb09e0f
tehcneko <chsqwyx@gmail.com>
2025-08-11 22:17:10 +0800

    Hide posts search for non-premium users

--- TMessagesProj/src/main/java/org/telegram/ui/Components/SearchViewPager.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/SearchViewPager.java
@@ -1366,7 +1366,9 @@ public class SearchViewPager extends ViewPagerFixed implements FilteredSearchVie
             }
             items.add(new Item(CHANNELS_TYPE));
             items.add(new Item(BOTS_TYPE));
-            items.add(new Item(POSTS_TYPE));
+            if (UserConfig.getInstance(currentAccount).isPremium()) {
+                items.add(new Item(POSTS_TYPE));
+            }
             if (!showOnlyDialogsAdapter) {
                 Item item = new Item(FILTER_TYPE);
                 item.filterIndex = 0;
@@ -1475,7 +1477,7 @@ public class SearchViewPager extends ViewPagerFixed implements FilteredSearchVie
             if (items.get(position).type == POSTS_TYPE) {
                 return 6;
             }
-            return items.get(position).type + position;
+            return /* items.get(position).type */ 10 + position;
         }
 
         @Override
