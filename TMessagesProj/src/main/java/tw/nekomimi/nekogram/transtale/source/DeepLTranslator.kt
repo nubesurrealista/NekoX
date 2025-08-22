d25e7c20f7056d10b02e7517a139a81ee03800eb
Nube 云 <136946098+nubesurrealista@users.noreply.github.com>
2025-08-22 17:09:22 -0400

    Update DeepLTranslator.kt

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/DeepLTranslator.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/transtale/source/DeepLTranslator.kt
@@ -9,18 +9,21 @@ import tw.nekomimi.nekogram.transtale.deepl.DeepLTranslatorRaw
 
 object DeepLTranslator : Translator {
 
-    val targetLanguages = listOf("DE", "EN", "ES", "FR", "IT", "JA", "NL", "PL", "PT", "RU", "ZH")
+    val targetLanguages = listOf(
+        "AR", "BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR",
+        "HE", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL",
+        "PT", "RO", "RU", "SK", "SL", "SV", "TH", "TR", "UK", "VI", "ZH"
+    )
 
     val client = DeepLTranslatorRaw()
 
     override suspend fun doTranslate(from: String, to: String, query: String): String {
-
         if (to !in targetLanguages) {
-
             throw UnsupportedOperationException(LocaleController.getString(R.string.TranslateApiUnsupported))
-
         }
 
-        return withContext(Dispatchers.IO) { client.translate(query, "auto", to) }
+        return withContext(Dispatchers.IO) {
+            client.translate(query, if (from.isEmpty()) "AUTO" else from, to)
+        }
     }
 }
