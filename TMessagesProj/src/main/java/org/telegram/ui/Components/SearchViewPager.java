21d6ae112de55f6133efc25d6eff509e6359e116
tehcneko <chsqwyx@gmail.com>
2025-09-02 19:55:56 +0800

    Always show search pager

--- TMessagesProj/src/main/java/org/telegram/ui/Components/SearchViewPager.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/SearchViewPager.java
@@ -1330,7 +1330,7 @@ public class SearchViewPager extends ViewPagerFixed implements FilteredSearchVie
     }
 
     public void showDownloads() {
-        setPosition((expandedPublicPosts ? 1 : 0) + 5);
+        setPosition((expandedPublicPosts ? 1 : 0) + (UserConfig.getInstance(currentAccount).isPremium() ? 5 : 4));
     }
 
     public int getPositionForType(int initialSearchType) {
