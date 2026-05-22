5d14f4a41333ffd754222b3356f553e57b1bfefa
dic1911 <d69873@gmail.com>
2026-05-23 00:07:08 +0800

    fix UI

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBarLayout.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBarLayout.java
@@ -3497,7 +3497,7 @@ public class ActionBarLayout extends FrameLayout implements INavigationLayout, F
         if (lastWindowInsetsCompat != null) {
             dispatchApplyWindowInsetsInternal(child, lastWindowInsetsCompat);
         }
-        if (bottomSheetTabs != null && indexOfChild(bottomSheetTabs) < getChildCount() - 1) {
+        if (bottomSheetTabs != null && indexOfChild(bottomSheetTabs) < getChildCount() - 1 && !bottomSheetTabs.getTabs().isEmpty()) {
             bottomSheetTabs.bringToFront();
         }
     }
