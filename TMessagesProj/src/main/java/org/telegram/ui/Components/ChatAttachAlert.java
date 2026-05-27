19c1d93a4ccf48ca704b39de56f1442a6eb0937f
dic1911 <d69873@gmail.com>
2026-05-27 13:07:55 +0800

    fix: patch new ai btn spots

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatAttachAlert.java
@@ -5932,7 +5932,7 @@ public class ChatAttachAlert extends BottomSheet implements NotificationCenter.N
 
     private boolean shownAiButton;
     private void showAiButton(boolean show_) {
-        final boolean show = show_ && (baseFragment instanceof ChatActivity && !((ChatActivity) baseFragment).isSecretChat());
+        final boolean show = show_ && (baseFragment instanceof ChatActivity && !((ChatActivity) baseFragment).isSecretChat()) && !MomoConfig.hideAIEditButton.Bool();
 
         if (shownAiButton == show) return;
         if (show) {
